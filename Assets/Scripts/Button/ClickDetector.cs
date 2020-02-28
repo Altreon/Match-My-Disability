8e701e87a4d1bf0b7910b8a29b540f1e135f87cb
Matteo BRANDI <matteo.brandi@ensiie.fr>
2020-02-28 22:46:36 +0100

    ajout d'un press button basique

---
+++ Assets/Scripts/Button/ClickDetector.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickDetector : MonoBehaviour
+{
+    PressButton button;
+
+    void Start () {
+        button = transform.parent.GetComponent<PressButton>();
+    }
+    void OnCollisionEnter(Collision collision) {
+        if(collision.transform.tag == "Button") {
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Button/PressButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PressButton : MonoBehaviour
+{
+    public UnityEvent clickEvent;
+    
+    void click () {
+        clickEvent.Invoke();
+    }
+}
