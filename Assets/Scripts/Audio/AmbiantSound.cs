c97d15e3628440e14873df5e3ead4f0bfb71b31b
Matteo BRANDI <matteo.brandi@ensiie.fr>
2020-02-29 15:45:38 +0100

    ajout de tout un tas de son pour les collision entre objet, + sripts de gestion des sons de collision, + son ambiant. Manque la spatialization des sons.

---
+++ Assets/Scripts/Audio/AmbiantSound.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbiantSound : MonoBehaviour
+{
+    public AudioClip clip;
+    public bool autoStart = true;
+    [Range(0f, 1f)]
+    public float volume = 1f;
+    [Range(-3f, 3f)]
+    public float pitch = 1f;
+    
+    AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+        audioSource.clip = clip;
+        audioSource.loop = true;
+        audioSource.spatialBlend = 0f;
+
+        if(autoStart){
+            audioSource.Play();
+        }
+
+        audioSource.volume = volume;
+        audioSource.pitch = pitch;
+
+        PlaySound();
+    }
+
+    public void PlaySound () {
+        audioSource.Play();
+        audioSource.PlayOneShot(clip);
+    }
+
+    public void StopSound () {
+        audioSource.Stop();
+    }
+}
