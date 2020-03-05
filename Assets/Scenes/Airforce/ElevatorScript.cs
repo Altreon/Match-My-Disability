31a51ceecd20a9af62dc0e32707f9d9a1e2680c6
Lucas Herfort <lucas.herfort@gmail.com>
2020-03-05 14:22:27 +0100

    Ajout animations Elevator

---
+++ Assets/Scenes/Airforce/ElevatorScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class ElevatorScript : MonoBehaviour
+{
+    public Animator anim;
+    // Start is called before the first frame update
+    private void Awake() 
+    {
+        anim = GetComponent<Animator>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
