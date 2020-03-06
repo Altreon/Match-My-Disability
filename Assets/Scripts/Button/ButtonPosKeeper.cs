7d82e3f815486211f124f90ed3bc43e18ae35d75
Matteo BRANDI <matteo.brandi@ensiie.fr>
2020-03-06 23:55:35 +0100

    fix new button fix

--- Assets/Scripts/Button/ButtonPosKeeper.cs
+++ Assets/Scripts/Button/ButtonPosKeeper.cs
@@ -9,21 +9,37 @@ public class ButtonPosKeeper : MonoBehaviour
 
     float normalDist;
     Vector3 originalPos;
+    Rigidbody rb;
+    bool locked = false;
     // Start is called before the first frame update
     void Start()
     {
         normalDist = Vector3.Distance(buttonLimit.position, transform.position);
         originalPos = transform.position;
+
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         float distFromLimit = Vector3.Distance(buttonLimit.position, transform.position);
         float distFromOrigin = Vector3.Distance(originalPos, transform.position);
 
-        if(distFromLimit > normalDist + supOffset || distFromOrigin > normalDist){
-            transform.position = originalPos;
+        if(!locked && (distFromLimit > normalDist + supOffset || distFromOrigin > normalDist)){
+            //transform.position = originalPos;
+            rb.constraints = RigidbodyConstraints.FreezeAll;
+            locked = true;
         }
     }
+
+    void OnCollisionExit(Collision collision) {
+        if(!locked){
+            return;
+        }
+
+         //if (collision.gameObject.tag == "Hand") {
+             rb.constraints &= ~RigidbodyConstraints.FreezePositionY; 
+         //}
+     }
 }
