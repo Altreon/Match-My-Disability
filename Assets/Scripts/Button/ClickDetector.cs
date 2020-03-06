e907bc038a8ba4e7e460fad691d48c90ad57e80b
Matteo BRANDI <matteo.brandi@ensiie.fr>
2020-03-06 23:59:58 +0100

    la c'est fix le boutton

--- Assets/Scripts/Button/ClickDetector.cs
+++ Assets/Scripts/Button/ClickDetector.cs
@@ -11,7 +11,7 @@ public class ClickDetector : MonoBehaviour
     }
     void OnCollisionEnter(Collision collision) {
         if(collision.transform.tag == "Button") {
-            //button.click();
+            button.click();
         }
     }
 }
