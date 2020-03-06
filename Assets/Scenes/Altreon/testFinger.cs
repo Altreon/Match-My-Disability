0444798a271eabc342e2bf4272a37f949818ecdd
Matteo BRANDI <matteo.brandi@ensiie.fr>
2020-03-06 19:31:22 +0100

    fix button sur la scene altreon + fix offset de la tasse de café

---
+++ Assets/Scenes/Altreon/testFinger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testFinger : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y - speed * Time.deltaTime, transform.position.z);
+        //transform.position.y -= 20;
+    }
+}
