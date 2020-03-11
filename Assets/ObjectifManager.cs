14dab5fdeda34071e55d12ad282fd087f7d5e7b6
JALIK <jalik.metagram@gmail.com>
2020-03-11 21:10:30 +0100

    Création du niveau de base, ajout d'un canvas qui marche pas et de l'objectif manager de base, en théorie, mettre le café et activer la machine active un objectif mais plusieurs bugs : :one: la tasse s'envole dans la machine (????) :two: le bouton vertical marche pas (on déplace le bouton sur un plan et pas en profondeur) et :three: on peut pas voir le résultat à cause du canvas et du bouton qui bug. On peut normalement poser le notebook sur son bureau

---
+++ Assets/ObjectifManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectifManager : MonoBehaviour
+{
+
+    private static ObjectifManager _instance = null;
+    public static ObjectifManager Instance
+    {
+        get { return _instance; }
+    }
+
+    [SerializeField] private GameObject objectifCoffee;
+    [SerializeField] private GameObject objectifNotepad;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (_instance == null)
+            _instance = this;
+        else
+        {
+            Debug.LogError("Can't have 2 OvjectifManager. Destroying second");
+            Destroy(gameObject);
+        }
+    }
+
+    public void setObjectif(string name)
+    {
+        if(name.Equals("coffee"))
+            objectifCoffee.SetActive(true);
+        else if(name.Equals("note"))
+            objectifNotepad.SetActive(true);
+        else
+        {
+            Debug.LogError("Unkown name : " + name + ", please use 'coffee' or 'note'");
+        }
+    }
+
+}
---
+++ Assets/Scenes/Jalik/CoffeeMachine.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoffeeMachine : MonoBehaviour
+{
+    private bool cupIsPresent = false;
+
+    [SerializeField] private GameObject particles;
+    
+    public void SetCupIsPresent()
+    {
+        cupIsPresent = true;
+    }
+
+    public void ButtonPressed()
+    {
+        if (cupIsPresent)
+        {
+            //Play sound coffee
+            particles.SetActive(true);
+            ObjectifManager.Instance.setObjectif("coffee");
+        }
+        else
+        {
+            //Play sound error
+        }
+    }
+}
