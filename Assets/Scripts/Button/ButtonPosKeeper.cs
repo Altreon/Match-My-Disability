f4bc95cbb28c1ba5a2dd32f6e1bdcbb4f3c9ef91
Brandi Matteo <matteo.brandi.pro@gmail.com>
2020-03-12 15:42:46 +0100

    new button pos keeper system

--- Assets/Scripts/Button/ButtonPosKeeper.cs
+++ Assets/Scripts/Button/ButtonPosKeeper.cs
@@ -8,25 +8,28 @@ public class ButtonPosKeeper : MonoBehaviour
 	
     public Transform buttonLimit;
     public Transform downButton;
-    public float supOffset = 0.03f;
+	public Transform upButton;
+    //public float supOffset = 0.03f;
+	public Transform downLimit;
+	public Transform upLimit;
 	
 	public Axis axisConstraint = Axis.X;
+	public bool invert;
 
-    float normalDist;
-    Vector3 originalPos;
+    //float normalDist;
+    //Vector3 originalPos;
     Rigidbody rb;
     bool locked = false;
     // Start is called before the first frame update
     void Start()
     {
-        normalDist = Vector3.Distance(buttonLimit.position, downButton.position);
-        originalPos = transform.position;
+        /*normalDist = Vector3.Distance(buttonLimit.position, downButton.position);
+        originalPos = transform.position;*/
 
         rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    /*void Update()
     {
         float distFromLimit = Vector3.Distance(buttonLimit.position, downButton.position);
         //float distFromOrigin = Vector3.Distance(buttonLimit.position, transform.position);
@@ -53,6 +56,34 @@ public class ButtonPosKeeper : MonoBehaviour
 				case Axis.Y : rb.constraints &= ~RigidbodyConstraints.FreezePositionY; break;
 				case Axis.Z : rb.constraints &= ~RigidbodyConstraints.FreezePositionZ; break;
 			 }
-         //}
-     }
+         }
+    }*/
+	
+	void FixedUpdate () {
+		float distanceUp;
+		float distanceDown;
+		switch (axisConstraint) {
+			case Axis.X : 
+				distanceUp = upLimit.position.x - upButton.position.x;
+				distanceDown = downButton.position.x - downLimit.position.x;
+				if(invert) {distanceUp *= -1; distanceDown *= -1;}
+				if(distanceUp < 0) {transform.Translate(Vector3.up * distanceUp);}
+				if(distanceDown < 0) {transform.Translate(Vector3.up * -distanceDown);}
+				break;
+			case Axis.Y : 
+				distanceUp = upLimit.position.y - upButton.position.y;
+				distanceDown = downButton.position.y - downLimit.position.y;
+				if(invert) {distanceUp *= -1; distanceDown *= -1;}
+				if(distanceUp < 0) {transform.Translate(Vector3.up * distanceUp);}
+				if(distanceDown < 0) {transform.Translate(Vector3.up * -distanceDown);}
+				break;
+			case Axis.Z : 
+				distanceUp = upLimit.position.z - upButton.position.z;
+				distanceDown = downButton.position.z - downLimit.position.z;
+				if(invert) {distanceUp *= -1; distanceDown *= -1;}
+				if(distanceUp < 0) {transform.Translate(Vector3.up * distanceUp);}
+				if(distanceDown < 0) {transform.Translate(Vector3.up * -distanceDown);}
+				break;
+		}
+	}
 }
