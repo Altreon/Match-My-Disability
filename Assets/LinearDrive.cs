8167a8114982374376ca54b4762bd70da62753a4
Lucas Herfort <lucas.herfort@gmail.com>
2020-03-19 17:36:52 +0100

    MAJ placard

--- Assets/LinearDrive.cs
+++ Assets/LinearDrive.cs
@@ -4,26 +4,30 @@ using UnityEngine;
 
 public class LinearDrive : MonoBehaviour
 {
+    private float close;
+    private float open;
     [SerializeField] 
-    private float close = -10.2f;
-    [SerializeField] 
-    private float open = -9.739f;
+    private float decalage =  0.287f;
     private Vector3 lastPos;    
     [SerializeField]
     Transform door;
     void Start()
     {
+        close = transform.position.x;
+        open = close + 0.442f;
         lastPos = transform.position;
         GetComponent<BoxCollider>().enabled = true;
     }
 
     // Update is called once per frame
+
+    /*
     public void Grab()
     {
         if(lastPos != transform.position)
         {
             var x = door.transform.position;
-            x.x = transform.position.x - 0.287f;
+            x.x = transform.position.x - decalage;
             door.transform.position = x;
             lastPos = transform.position;
         }
@@ -36,7 +40,7 @@ public class LinearDrive : MonoBehaviour
             lastPos = transform.position; 
 
             var x = door.transform.position;
-            x.x = transform.position.x - 0.287f;
+            x.x = transform.position.x - decalage;
             door.transform.position = x;
             lastPos = transform.position;
         }
@@ -49,7 +53,7 @@ public class LinearDrive : MonoBehaviour
             lastPos = transform.position; 
 
             var x = door.transform.position;
-            x.x = transform.position.x - 0.287f;
+            x.x = transform.position.x - decalage;
             door.transform.position = x;
             lastPos = transform.position;
         }
@@ -58,5 +62,43 @@ public class LinearDrive : MonoBehaviour
     public void Drop()
     {
         transform.position = door.transform.position + new Vector3(0.275f,0.028f,0f);
+    }
+    */
+
+    void Update()
+    {
+        if(lastPos != transform.position)
+        {
+            var x = door.transform.position;
+            x.x = transform.position.x - decalage;
+            door.transform.position = x;
+            lastPos = transform.position;
+        }
+
+        if(transform.position.x < close)
+        {
+            var pos = transform.position;
+            pos.x = close;
+            transform.position = pos;
+            lastPos = transform.position; 
+
+            var x = door.transform.position;
+            x.x = transform.position.x - decalage;
+            door.transform.position = x;
+            lastPos = transform.position;
+        }
+
+        if(transform.position.x > open)
+        {
+            var pos = transform.position;
+            pos.x = open;
+            transform.position = pos;
+            lastPos = transform.position; 
+
+            var x = door.transform.position;
+            x.x = transform.position.x - decalage;
+            door.transform.position = x;
+            lastPos = transform.position;
+        }
     }
 }
