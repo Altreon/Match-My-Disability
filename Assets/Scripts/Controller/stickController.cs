0f8b52d7d514b2d1cc6c9516b18d7b2c8acbabbc
Matteo BRANDI <matteo.brandi@ensiie.fr>
2020-03-26 18:08:37 +0100

    test new chair control

--- Assets/Scripts/Controller/stickController.cs
+++ Assets/Scripts/Controller/stickController.cs
@@ -4,16 +4,60 @@ using UnityEngine;
 
 public class stickController : MonoBehaviour
 {
-    static public bool controlledByRight = false;
-    static public bool controlledByLeft = false;
+    //static public bool controlledByRight = false;
+    //static public bool controlledByLeft = false;
+    static public bool controlled = false;
 
-    public bool iAmRight = true;
+    public WheelchairController chairController;
+    public Renderer handRenderer;
+    public Renderer fakeHandRenderer;
+    [SerializeField] private float deadzone = 0.25f;
+    [SerializeField] private float rotateStick = 10f;
 
-    public void control(bool controlled){
-        if(iAmRight){
-            controlledByLeft = controlled;
+    public void Control(bool control){
+        controlled = control;
+
+        if(control){
+            handRenderer.enabled = false;
+            fakeHandRenderer.enabled = true;
         }else{
-            controlledByLeft = controlled;
+            handRenderer.enabled = true;
+            fakeHandRenderer.enabled = false;
         }
+    }
+
+    void Update () {
+        
+        if(!controlled){
+            return;
+        }
+
+        Vector2 axis = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
+
+        if (axis.magnitude < deadzone){
+            return;
+        }
+
+        if (Mathf.Abs(axis.y) > Mathf.Abs(axis.x))
+        {
+            //Déplacement vertical
+            if(axis.y > 0f){
+                transform.rotation = transform.parent.rotation * Quaternion.Euler(0, 0, rotateStick);
+            }else{
+                transform.rotation = transform.parent.rotation * Quaternion.Euler(0, 0, -rotateStick);
+            }
+        }
+        else
+        {
+            //Rotation
+            if(axis.x > 0f){
+                transform.rotation = transform.parent.rotation * Quaternion.Euler(rotateStick, 0, 0);
+            }
+            else{
+                transform.rotation = transform.parent.rotation * Quaternion.Euler(-rotateStick, 0, 0);
+            }
+        }
+
+        chairController.Move(axis);
     }
 }
